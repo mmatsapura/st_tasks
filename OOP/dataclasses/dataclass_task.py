ad1ab3f27e186fb81d68aae4f73ff6f3efdc1b1c
zllrocky <dreambot008@gmail.com>
2026-09-11 14:14:34 +0300

    Решение задания dataclass

--- OOP/dataclasses/dataclass_task.py
+++ OOP/dataclasses/dataclass_task.py
@@ -1,4 +1,31 @@
-#
+from dataclasses import dataclass, field
+
+
+@dataclass(frozen=True)
+class Book:
+    title: str
+    author: str
+    year: int
+    pages: int = field(default=100)
+
+    def __post_init__(self):
+        if self.year < 0:
+            raise ValueError('Год не может быть отрицательным числом')
+
+
+
+book1 = Book('Learn Python', 'John Smith', 2023)
+book2 = Book('Harry Potter', 'Vanilla', 2010, 565)
+book3 = Book('Harry Potter', 'Vanilla', 2010, 565)
+# book4 = Book('Harry', 'Malina', -2010)
+
+print(book1)
+print(book2)
+
+print(book1 == book2)
+print(book3 == book2)
+
+
 # Создай dataclass Book, который будет хранить информацию о книге.
 # Требования:
 # Класс должен содержать следующие поля:
