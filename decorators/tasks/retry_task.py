57b66e264ce1a7ac88572847dabb1b103ab04c2b
zllrocky <dreambot008@gmail.com>
2026-09-18 16:30:25 +0300

    Задание декоратор retry(times)

--- decorators/tasks/retry_task.py
+++ decorators/tasks/retry_task.py
@@ -1,10 +1,30 @@
 '''
-Напиши декоратор retry(times), который повторяет выполнение функции при возникновении исключения.
+Напиши декоратор retry(times), который повторяет выполнение функции
+при возникновении исключения.
 times — количество попыток.
 Если все попытки неудачны, декоратор выбрасывает последнее исключение.'''
 
 
-# @retry(times=3)
+from functools import wraps
+
+
+def retry(times):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            last_error = None
+            for _ in range(times):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    last_error = e
+                    print(f'Ошибка {e}')
+            raise last_error
+        return wrapper
+    return decorator
+
+
+@retry(times=3)
 def risky_action():
     import random
     if random.random() < 0.7:
