eb7f362fc82d32fc415b6de63e34951ad3bb37e1
zllrocky <dreambot008@gmail.com>
2026-09-21 14:08:26 +0300

    Решение access_control_task.py

--- decorators/tasks/access_control_task.py
+++ decorators/tasks/access_control_task.py
@@ -24,27 +24,62 @@ Call limit exceeded
 180.0
 """
 
-from functools import wraps
-
-
 CURRENT_USER = {"logged_in": False}
-
 
 # --- напиши require_login, call_limit, discount здесь ---
 
+from functools import wraps
 
-# @require_login
+
+def require_login(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if False in CURRENT_USER.values():
+            return 'Access denied'
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def call_limit(func = None, times: int = 0):
+    def decorator(f):
+        count_times = 0
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            nonlocal count_times
+            if count_times >= times:
+                return 'Call limit exceeded'
+            count_times += 1
+            return f(*args, **kwargs)
+        return wrapper
+    if func is None:
+        return decorator
+    return decorator(func)
+
+
+def discount(func = None, percent: int = 0):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            results = f(*args, **kwargs)
+            return results * (100 - percent) / 100
+        return wrapper
+    if func is None:
+        return decorator
+    return decorator(func)
+
+
+@require_login
 def add_to_cart(item):
     return f"{item} added to cart"
 
 
-# @call_limit(times=2)
-# @require_login
+@call_limit(times=2)
+@require_login
 def checkout():
     return "order paid"
 
 
-# @discount(percent=10)
+@discount(percent=10)
 def apply_welcome_bonus():
     return 200
 
